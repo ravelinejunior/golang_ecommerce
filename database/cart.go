1c53054fba99ce20ae29b86d223fc98bddaa9196
ravelinejunior <junior.raveline@gmail.com>
2023-12-18 21:37:49 +0000

    creating function to add product to cart

--- database/cart.go
+++ database/cart.go
@@ -1,6 +1,15 @@
 package database
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/ravelinejunior/golang_ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 var (
 	ErrCantFindProduct    = errors.New("can't find the product")
@@ -12,9 +21,44 @@ var (
 	ErrCantBuyCartItem    = errors.New("can't update the purchase")
 )
 
-func AddProductToCart() {
+// AddProductToCart adds a product to the cart of a user
+func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	// search the product collection for the given product id
+	searchFromDB, err := prodCollection.Find(ctx, bson.M{"_id": productID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
 
+	// decode the products into a slice of ProductUser structs
+	var productCart []models.ProductUser
+	err = searchFromDB.All(ctx, &productCart)
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
+
+	// convert the user id to a primitive.ObjectID
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdsNotValid
+	}
+
+	// create a filter to search for the given user id
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+
+	// create an update to add the products to the user's cart
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}}
+
+	// update the user document with the new cart items
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+	return nil
 }
+
 func RemoveCartItem() {
 
 }
